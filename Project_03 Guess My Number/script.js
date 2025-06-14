3c0615a10d14ef3b8b2d3130aabdf11c8c541f65
Priya Mehta <pmehta12122005@gmail.com>
2025-06-14 16:13:20 +0530

    Guess_My_Number

--- Project_03 Guess My Number/script.js
+++ Project_03 Guess My Number/script.js
@@ -1,45 +1,28 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = '👏Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
-
-// Computing the Secret number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-// Defining the score and high score
 let score = 20;
 let highScore = 0;
 
-// Initialising the check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  // When there is no Input
+
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
 
-    // When there is correct Input
+   
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👏 Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
 
-    // Setting the highScore
     if (highScore < score) {
       highScore = score;
     }
     document.querySelector('.highscore').textContent = highScore;
 
-    // Changing the color for the background when the answer is correct
+   
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     // Changing the size of the number when the correct answer
